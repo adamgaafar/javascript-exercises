d3126d0bf463d2622cdb596576e7a976e9009ab9
adamgaafar <dodogaafar.ag@gmail.com>
2025-02-17 21:52:53 +0200

    last odin exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return "ERROR"
+    } else if (a < 0 || b < 0) {
+        return "ERROR"
+    } else if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR"
+    } else {
+        let start = Math.min(a, b);
+        let end = Math.max(a, b);
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
 
+        return sum;
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,26 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+    if (f == 32) {
+        return 0
+    } else {
+        let x = (f - 32) * (5 / 9);
+        x = (Math.round(x * 10) / 10)
+        return x;
+    }
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+    if (c == 0) {
+        return 32
+    } else {
+        let x = (c * (9 / 5) + 32);
+        x = (Math.round(x * 10) / 10)
+        return x;
+    }
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit
 };
