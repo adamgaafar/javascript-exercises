ec6b9035afab85c35fa21be678c4bfff3f26fe61
adamgaafar <dodogaafar.ag@gmail.com>
2025-02-17 20:18:53 +0200

    first 2 exs

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(name, num) {
+    if (num < 0) return "ERROR";
 
+    let result = '';
+    for (let i = 0; i < num; i++) {
+        result += name;
+    }
+    return result;
 };
 
 // Do not edit below this line
